9d61450d83cb10e8fa224c9941a0d38e2caa08be
marikomands <marikomands@gmail.com>
2021-11-24 11:54:01 +0000

    including options for colors

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
+import reactDom from "react-dom";
 
 const items = [
   {
@@ -17,10 +19,25 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: "the Color Red",
+    value: "red",
+  },
+  {
+    label: "the Color Green",
+    value: "Green",
+  },
+  {
+    label: "A Shade of Blue",
+    value: "Blue",
+  },
+];
+
 export default () => {
   return (
     <div>
-      <Search />
+      <Dropdown options={options} />
     </div>
   );
 };
