8bedfa6e071987e3dc9d72dddf7f5bf3f5ba0d08
marikomands <marikomands@gmail.com>
2021-12-07 11:39:40 +0000

    Added "Toggle Dropdown" button

--- src/App.js
+++ src/App.js
@@ -36,14 +36,23 @@ const options = [
 
 export default () => {
   const [selected, setSelected] = useState(options[0]);
+  const [showDropdown, setShowDropdown] = useState(true);
+  console.log("🚀 ~ showDropdown", showDropdown);
+  console.log("🚀 ~ setShowDropdown", setShowDropdown);
 
   return (
     <div>
-      <Dropdown
-        selected={selected}
-        onSelectedChange={setSelected}
-        options={options}
-      />
+      <button onClick={() => setShowDropdown(!showDropdown)}>
+        Toggle Dropdown
+      </button>
+
+      {showDropdown ? (
+        <Dropdown
+          selected={selected}
+          onSelectedChange={setSelected}
+          options={options}
+        />
+      ) : null}
     </div>
   );
 };
